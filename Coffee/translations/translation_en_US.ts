56f3ca1595d89499c5e39b7ebf5e169bed8821d7
Pierre Fribourg <pfribourg@softbankrobotics.com>
2018-10-10 15:58:53 +0200

    first trial

---
+++ Coffee/translations/translation_en_US.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello Konica Minolta</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Konica Minolta</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>oh i will play with coffee</source>
+            <comment>Text</comment>
+            <translation type="unfinished">oh i will play with coffee</translation>
+        </message>
+    </context>
+</TS>
